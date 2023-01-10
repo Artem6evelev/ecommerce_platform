b2a5e5e4431dcd4714d2ce252ea76790fba199b5
Artem6evelev <artem6evelev92@gmail.com>
2023-01-10 13:18:35 +0200

    set context for state managing user sign in

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<Authentification />} />
+        <Route path="auth" element={<Authentification />} />
       </Route>
     </Routes>
   );
