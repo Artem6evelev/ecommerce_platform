9f2b0b60575ee7f44c26bbf781a95901710174d0
Artem6evelev <artem6evelev92@gmail.com>
2023-03-05 21:30:22 +0200

    redux thunk and spinner

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,19 +1,28 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/category.selector";
+import Spinner from "../../components/spinner/spinnes.component";
 
 import CategoryPreview from "../../components/category-preview/category-preview-component";
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((key) => {
-        const products = categoriesMap[key];
-        return <CategoryPreview key={key} title={key} products={products} />;
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((key) => {
+          const products = categoriesMap[key];
+          return <CategoryPreview key={key} title={key} products={products} />;
+        })
+      )}
     </Fragment>
   );
 };
