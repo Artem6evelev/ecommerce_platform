0ede4b502d73e7e29719f7bb904e7792afedd639
Artem6evelev <artem6evelev92@gmail.com>
2023-03-10 21:37:30 +0200

    setup redux tollkit

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,4 +1,17 @@
-import { combineReducers } from "redux";
+// import { combineReducers } from "redux";
+
+// import { userReducer } from "./user/user.reducer";
+// import { categoriesReducer } from "./categories/category.reducer";
+// import { cartReducer } from "./cart/cart.reducer";
+
+// export const rootReducer = combineReducers({
+//   user: userReducer,
+//   categories: categoriesReducer,
+//   cart: cartReducer,
+// });
+
+//* USING REDUX TOOLKIT
+import { combineReducers } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
--- src/store/store.js
+++ src/store/store.js
@@ -1,43 +1,57 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { compose, createStore, applyMiddleware } from "redux";
 // import logger from "redux-logger";
-import createSagaMiddleware from "@redux-saga/core";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// // import logger from "redux-logger";
+// import createSagaMiddleware from "@redux-saga/core";
 
-import { rootSaga } from "./root-saga";
+// import { rootSaga } from "./root-saga";
 
+// import { rootReducer } from "./root-reducer";
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist: ["user"],
+// };
+
+// const sagaMiddleware = createSagaMiddleware();
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const middleWares = [
+//   process.env.NODE_ENV !== "production" && logger,
+//   sagaMiddleware,
+// ].filter(Boolean);
+
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLD_EXTENSION_COMPOSE__) ||
+//   compose;
+
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
+
+// sagaMiddleware.run(rootSaga);
+
+// export const persistor = persistStore(store);
+
+//* USING REDUX TOOLKIT
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
+import logger from "redux-logger";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["user"],
-};
-
-const sagaMiddleware = createSagaMiddleware();
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middleWares = [
-  process.env.NODE_ENV !== "production" && logger,
-  sagaMiddleware,
-].filter(Boolean);
-
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLD_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
+const middleWares = [process.env.NODE_ENV !== "development" && logger].filter(
+  Boolean
 );
 
-sagaMiddleware.run(rootSaga);
-
-export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: middleWares,
+});
